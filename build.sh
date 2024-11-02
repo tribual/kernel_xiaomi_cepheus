56fe58bbe671fdb3eb12cf27d5d2e6dcb54230b7
¨raystef66¨ <s.vanbarel@gmail.com>
2024-11-02 08:54:01 +0100

    InfiniR Cepheus v1.34 * merge 4.14.354 & 355 openela

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@
 
 KERNEL_DEFCONFIG=cepheus_defconfig
 ANYKERNEL3_DIR=$PWD/AnyKernel3/
-FINAL_KERNEL_ZIP=InfiniR_cepheus_v1.33.zip
+FINAL_KERNEL_ZIP=InfiniR_cepheus_v1.34.zip
 
 # paths
 TC="/home/raystef66/kernel/prebuilts"
